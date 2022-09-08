3dbbd83d77d666c7faf06d3924760fbb0b55dbb1
Jasminoza <testbot91@gmail.com>
2022-09-08 16:02:59 +0300

    HibernateUserRepositoryImpl was implemented

--- src/main/java/org/yolkin/repository/hibernate/HibernateUserRepositoryImpl.java
+++ src/main/java/org/yolkin/repository/hibernate/HibernateUserRepositoryImpl.java
@@ -1,33 +1,70 @@
 package org.yolkin.repository.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.yolkin.model.User;
 import org.yolkin.repository.UserRepository;
+import org.yolkin.util.HibernateSessionFactoryUtil;
 
 import java.util.List;
 
 public class HibernateUserRepositoryImpl implements UserRepository {
     @Override
     public List<User> getAll() {
-        return null;
+        try (Session session = getSession()) {
+            return session.createQuery("From User", User.class).list();
+        }
     }
 
     @Override
     public User create(User user) {
-        return null;
+        return saveUserToDB(user);
     }
 
     @Override
-    public User getById(Long aLong) {
-        return null;
+    public User getById(Long id) {
+        try (Session session = getSession()) {
+            return session.get(User.class, id);
+        }
     }
 
     @Override
     public User update(User user) {
-        return null;
+        return saveUserToDB(user);
     }
 
     @Override
-    public void delete(Long aLong) {
+    public void delete(Long id) {
+        Transaction transaction = null;
 
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            session.remove(getById(id));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    private User saveUserToDB(User user) {
+        Transaction transaction = null;
+
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+            user = session.merge(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+
+        return ((user.getId() != null) ? user : null);
+    }
+
+    private Session getSession(){
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession();
     }
 }
