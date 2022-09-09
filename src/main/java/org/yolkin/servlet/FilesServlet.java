e4f70ffe076876b4c3abf96a17f5dee64d8f396e
Jasminoza <testbot91@gmail.com>
2022-09-09 11:48:34 +0300

    FilesServlet doGet was implemented

--- src/main/java/org/yolkin/servlet/FilesServlet.java
+++ src/main/java/org/yolkin/servlet/FilesServlet.java
@@ -1,11 +1,8 @@
 package org.yolkin.servlet;
 
 import org.yolkin.model.File;
-import org.yolkin.model.User;
 import org.yolkin.repository.FileRepository;
-import org.yolkin.repository.UserRepository;
 import org.yolkin.repository.hibernate.HibernateFileRepositoryImpl;
-import org.yolkin.repository.hibernate.HibernateUserRepositoryImpl;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,5 +13,35 @@ import java.util.List;
 
 public class FilesServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try (PrintWriter writer = response.getWriter()) {
+            FileRepository fileRepository = new HibernateFileRepositoryImpl();
+            List<File> files = fileRepository.getAll();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.append("<!DOCTYPE = html>");
+            stringBuilder.append("<html>");
+            stringBuilder.append("<head><title>");
+            stringBuilder.append("<h1>Files list</h1>");
+            stringBuilder.append("</title></head>");
+
+            stringBuilder.append("<body>");
+            stringBuilder.append("<h1>FILES DATA</h1>");
+
+            for (File file : files) {
+                stringBuilder.append("File ID: " + file.getId());
+                stringBuilder.append("<br/>");
+                stringBuilder.append("File name: " + file.getName());
+                stringBuilder.append("<br/>");
+                stringBuilder.append("File date of uploading: " + file.getDateOfUploading());
+                stringBuilder.append("<br/>");
+                stringBuilder.append("<br/>");
+            }
+
+            stringBuilder.append("</body>");
+            stringBuilder.append("</html>");
+
+            writer.println(stringBuilder);
+        }
     }
 }
