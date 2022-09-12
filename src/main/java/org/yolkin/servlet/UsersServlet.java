427c7985d2d9856cdb67ce5e8bcdb08de422ea1d
Jasminoza <testbot91@gmail.com>
2022-09-12 13:23:30 +0300

    UsersServlet was refactored

--- src/main/java/org/yolkin/servlet/UsersServlet.java
+++ src/main/java/org/yolkin/servlet/UsersServlet.java
@@ -3,44 +3,32 @@ package org.yolkin.servlet;
 import org.yolkin.model.User;
 import org.yolkin.repository.UserRepository;
 import org.yolkin.repository.hibernate.HibernateUserRepositoryImpl;
+import org.yolkin.util.ServletHelper;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 public class UsersServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try (PrintWriter writer = response.getWriter()) {
-            UserRepository userRepository = new HibernateUserRepositoryImpl();
-            List<User> users = userRepository.getAll();
+        UserRepository userRepository = new HibernateUserRepositoryImpl();
 
-            StringBuilder stringBuilder = new StringBuilder();
+        ServletHelper helper = new ServletHelper(response);
 
-            stringBuilder.append("<!DOCTYPE = html>");
-            stringBuilder.append("<html>");
-            stringBuilder.append("<head><title>");
-            stringBuilder.append("<h1>Users list</h1>");
-            stringBuilder.append("</title></head>");
+        List<User> users = userRepository.getAll();
 
-            stringBuilder.append("<body>");
-            stringBuilder.append("<h1>USERS DATA</h1>");
+        helper.setResponseHead("Users list");
+        helper.addH1ToResponseBody("USERS DATA");
 
-            for (User user : users) {
-                stringBuilder.append("User ID: " + user.getId());
-                stringBuilder.append("<br/>");
-                stringBuilder.append("User name: " + user.getName());
-                stringBuilder.append("<br/>");
-                stringBuilder.append("<br/>");
-            }
-
-            stringBuilder.append("</body>");
-            stringBuilder.append("</html>");
-
-            writer.println(stringBuilder);
+        for (User user : users) {
+            helper.addToResponseBody("User ID: " + user.getId());
+            helper.addToResponseBody("User name: " + user.getName());
+            helper.addToResponseBody("<br/>");
         }
+
+        helper.sendResponse();
     }
 }
