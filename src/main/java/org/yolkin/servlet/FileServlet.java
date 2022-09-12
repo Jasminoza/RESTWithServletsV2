f16f5f70c3bfe86eb2e2426aa63c8d5a04ee24a1
Jasminoza <testbot91@gmail.com>
2022-09-12 09:45:26 +0300

    FileServlet doPost was implemented

--- src/main/java/org/yolkin/servlet/FileServlet.java
+++ src/main/java/org/yolkin/servlet/FileServlet.java
@@ -2,7 +2,6 @@ package org.yolkin.servlet;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.yolkin.model.File;
 import org.yolkin.repository.FileRepository;
@@ -14,10 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
 
 public class FileServlet extends HttpServlet {
     private FileRepository fileRepository;
@@ -119,7 +116,7 @@ public class FileServlet extends HttpServlet {
         File file = fileRepository.getById(idFromRequest);
 
         if (file == null) {
-           helper.sendBadRequestStatus("File not found.");
+            helper.sendBadRequestStatus("File not found.");
             return;
         }
 
@@ -137,74 +134,61 @@ public class FileServlet extends HttpServlet {
     }
 
     public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try (PrintWriter writer = response.getWriter()) {
-            StringBuilder stringBuilder = new StringBuilder();
-            Long idFromRequest;
+        ServletHelper helper = new ServletHelper(response);
 
-            try {
-                idFromRequest = Long.valueOf(request.getHeader("file_id"));
-            } catch (Exception e) {
-                response.setStatus(400);
-                stringBuilder.append("Incorrect file id.");
-                writer.println(stringBuilder);
-                return;
-            }
+        Long idFromRequest;
 
-            File file = fileRepository.getById(idFromRequest);
+        try {
+            idFromRequest = Long.valueOf(request.getHeader("file_id"));
+        } catch (Exception e) {
+            helper.sendBadRequestStatus("Incorrect file id.");
+            return;
+        }
 
-            if (file == null) {
-                response.setStatus(400);
-                stringBuilder.append("File not found.");
-                writer.println(stringBuilder);
-                return;
-            }
+        File file = fileRepository.getById(idFromRequest);
 
-            Date date = new Date();
-            file.setDateOfUploading(date);
+        if (file == null) {
+            helper.sendBadRequestStatus("File not found.");
+            return;
+        }
 
-            DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
-            diskFileItemFactory.setRepository(new java.io.File(PATH_FOR_UPLOADING));
-            diskFileItemFactory.setSizeThreshold(MAX_MEMORY_SIZE);
+        ServletFileUpload uploader = helper.setupUploader(PATH_FOR_UPLOADING, MAX_MEMORY_SIZE, MAX_FILE_SIZE);
 
-            ServletFileUpload upload = new ServletFileUpload(diskFileItemFactory);
-            upload.setSizeMax(MAX_FILE_SIZE);
-
-            List<FileItem> fileItems = upload.parseRequest(request);
-
-            Iterator<FileItem> iterator = fileItems.iterator();
-
-            stringBuilder.append("<!DOCTYPE = html>");
-            stringBuilder.append("<html>");
-            stringBuilder.append("<head><title>");
-            stringBuilder.append("<h1>File details</h1>");
-            stringBuilder.append("</title></head>");
+        try {
+            Iterator<FileItem> iterator = uploader.parseRequest(request).iterator();
+            helper.setResponseHead("File details");
 
             while (iterator.hasNext()) {
                 FileItem fileItem = iterator.next();
-                Date dateOfUpdate = new Date();
+                Date date = new Date();
                 if (!fileItem.isFormField()) {
 
-                    java.io.File realFile;
+                    java.io.File realFile = helper.getFileFromRequest(fileItem, PATH_FOR_UPLOADING, date);
 
-                    String fileName = fileItem.getName();
-                    if (fileName.lastIndexOf("\\") >= 0) {
-                        realFile = new java.io.File(PATH_FOR_UPLOADING + dateOfUpdate + " " +
-                                fileName.substring(fileName.lastIndexOf("\\")));
-                    } else {
-                        realFile = new java.io.File(PATH_FOR_UPLOADING + dateOfUpdate + " " +
-                                fileName.substring(fileName.lastIndexOf("\\") + 1));
+                    try {
+                        java.io.File oldFile = new java.io.File(PATH_FOR_UPLOADING + java.io.File.separator + file.getName());
+
+                        if (!oldFile.delete()) {
+                            helper.sendBadRequestStatus("Can't update file on hard drive.");
+                            return;
+                        } else {
+                            fileItem.write(realFile);
+                            file.setName(realFile.getName());
+                            file.setDateOfUploading(date);
+                            file = fileRepository.update(file);
+
+                            helper.setResponseBody("File " + file.getName() + " was updated successfully.");
+                            helper.closeResponseBodyTag();
+                            helper.closeResponseTag();
+                            helper.sendResponse();
+                        }
+                    } catch (Exception e) {
+                        helper.sendBadRequestStatus("Can't save file on hard drive.");
                     }
-                    fileItem.write(realFile);
-
-//                    fileRepository.update();
-
-
                 }
             }
         } catch (FileUploadException e) {
-            throw new RuntimeException(e);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            helper.sendBadRequestStatus("Wrong request.");
         }
     }
 }
