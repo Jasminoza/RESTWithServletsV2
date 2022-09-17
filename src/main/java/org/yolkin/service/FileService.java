5f5cf87fd14d7185c2b1aa528500442813be718c
Jasminoza <testbot91@gmail.com>
2022-09-17 11:38:48 +0300

    FileService has been refactored

--- src/main/java/org/yolkin/service/FileService.java
+++ src/main/java/org/yolkin/service/FileService.java
@@ -1,9 +1,5 @@
 package org.yolkin.service;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.yolkin.model.Event;
 import org.yolkin.model.File;
 import org.yolkin.repository.EventRepository;
 import org.yolkin.repository.FileRepository;
@@ -16,11 +12,7 @@ import org.yolkin.util.ServiceHelper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-
-import static javax.servlet.http.HttpServletResponse.*;
 
 public class FileService {
     private final FileRepository fileRepository;
@@ -74,42 +66,6 @@ public class FileService {
         } else {
             return null;
         }
-
-//
-//        ServletFileUpload uploader = helper.setupUploader(PATH_FOR_UPLOADING, MAX_MEMORY_SIZE, MAX_FILE_SIZE);
-//        try {
-//            Iterator<FileItem> iterator = uploader.parseRequest(req).iterator();
-//            while (iterator.hasNext()) {
-//                FileItem fileItem = iterator.next();
-//                Date date = new Date();
-//
-//                if (!fileItem.isFormField()) {
-//                    java.io.File realFile = helper.getFileFromRequest(fileItem, PATH_FOR_UPLOADING, date);
-//                    try {
-//                        File fileForDB = new File();
-//                        fileForDB.setName(realFile.getName());
-//                        fileForDB.setDateOfUploading(date);
-//                        fileForDB.setFilepath(realFile.getPath());
-//
-//                        file = fileRepository.update(fileForDB);
-//                        fileItem.write(realFile);
-//
-//                        Event event = new Event();
-////                                    event.setEvent();// TODO::
-//
-//                        resp.setStatus(SC_CREATED);
-//
-//                    } catch (Exception e) {
-//                        resp.sendError(SC_NOT_IMPLEMENTED, "Can't save file on hard drive");
-//                        return null;
-//                    }
-//                }
-//            }
-//        } catch (FileUploadException e) {
-//            resp.sendError(SC_NOT_ACCEPTABLE, "Can't upload file or size of all files exceeds " + MAX_FILE_SIZE / 1024 + " kb.");
-//        }
-//    }
-
 }
 
     public void delete(HttpServletRequest req, HttpServletResponse resp, String mappingUrl) throws IOException {
