eaac729621ed878707d1d22b1a03e7e063a14fed
Jasminoza <testbot91@gmail.com>
2022-09-25 16:23:09 +0300

    FileCreationDTO has been implemented

--- src/main/java/org/yolkin/dto/FileCreationDTO.java
+++ src/main/java/org/yolkin/dto/FileCreationDTO.java
@@ -1,4 +1,27 @@
 package org.yolkin.dto;
 
 public class FileCreationDTO {
+    private String name;
+    private String filePath;
+
+    public FileCreationDTO(String name, String filePath) {
+        this.name = name;
+        this.filePath = filePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
 }
