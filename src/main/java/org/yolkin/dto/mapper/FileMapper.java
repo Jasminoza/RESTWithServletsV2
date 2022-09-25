37a54e2324da9a9dfa946ef9c5a394bd6e508243
Jasminoza <testbot91@gmail.com>
2022-09-25 16:29:09 +0300

    FileMapper has been implemented

--- src/main/java/org/yolkin/dto/mapper/FileMapper.java
+++ src/main/java/org/yolkin/dto/mapper/FileMapper.java
@@ -1,4 +1,27 @@
 package org.yolkin.dto.mapper;
 
+import org.yolkin.dto.FileCreationDTO;
+import org.yolkin.dto.FileDTO;
+import org.yolkin.model.FileEntity;
+
 public class FileMapper {
+    public static FileEntity toFile(FileCreationDTO fileCreationDTO) {
+        FileEntity fileEntity = new FileEntity();
+        fileEntity.setName(fileCreationDTO.getName());
+        fileEntity.setFilepath(fileCreationDTO.getFilePath());
+        return fileEntity;
+    }
+
+    public static FileDTO toFileDTO(FileEntity fileEntity) {
+        return new FileDTO(fileEntity.getId(), fileEntity.getName(), fileEntity.getFilepath(), fileEntity.getEvents());
+    }
+
+    public static FileEntity toFile(FileDTO fileDTO) {
+        FileEntity fileEntity = new FileEntity();
+        fileEntity.setId(fileDTO.getId());
+        fileEntity.setName(fileDTO.getName());
+        fileEntity.setFilepath(fileDTO.getFilePath());
+        fileEntity.setEvents(fileDTO.getEvents());
+        return fileEntity;
+    }
 }
